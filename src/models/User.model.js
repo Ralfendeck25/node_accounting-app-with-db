dc112ce7ac95da528a526068ff2daa25774df9ef
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-06-07 04:14:31 -0300

    Update User.model.js

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -15,21 +15,9 @@ const User = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
   },
   {
-    tableName: 'users',
+    tableName: 'Users',
     timestamps: false,
   }
 );
