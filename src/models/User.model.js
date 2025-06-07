2805b0fbd46f7454b596eff2a41d9e36b3cc0833
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-06-07 04:02:37 -0300

    Update User.model.js

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,9 +1,37 @@
 'use strict';
 
 const { sequelize } = require('../db.js');
+const { DataTypes } = require('sequelize');
 
 const User = sequelize.define(
-  // your code goes here
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+    timestamps: false,
+  }
 );
 
 module.exports = {
